14499b9199b24dff468e3e0d3dbccebc812a2c71
Jiwhun Kim <jiwhun.kim@outlook.com>
2021-02-27 13:14:47 +0900

    domain-rds feature order implement

--- external-api/src/main/kotlin/com/helloworld/cart/v1/controller/CartController.kt
+++ external-api/src/main/kotlin/com/helloworld/cart/v1/controller/CartController.kt
@@ -2,19 +2,24 @@ package com.helloworld.cart.v1.controller
 
 import com.helloworld.cart.data.CartLineItemRequestDto
 import com.helloworld.cart.service.CartApplicationService
-import com.helloworld.common.exception.BadRequestException
 import com.helloworld.common.response.Response
 import com.helloworld.config.annotation.HelloworldUser
 import com.helloworld.data.cart.CartDto
 import com.helloworld.data.common.User
+import com.helloworld.order.service.OrderApplicationService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.net.URI
+import java.util.*
 
 @RestController(value = "CartControllerV1")
 @RequestMapping(
         value = ["/order/carts"],
         produces = ["application/vnd.helloworld.api.v1+json; charset=UTF-8"]
 )
-class CartController(val cartApplicationService: CartApplicationService) {
+class CartController(
+        private val cartApplicationService: CartApplicationService,
+        private val orderApplicationService: OrderApplicationService) {
     @GetMapping("/account")
     fun get(@RequestHeader(value = "Authenticated") authenticated: Long): Response<CartDto> {
         val cartDto = cartApplicationService.findByAccountId(authenticated)
@@ -29,5 +34,12 @@ class CartController(val cartApplicationService: CartApplicationService) {
     ): Response<CartDto> {
         val cartDto = cartApplicationService.upsert(user, cartId, cartLineItemRequestDto)
         return Response(cartDto)
+    }
+
+    @RequestMapping(path = ["/{cartId}/orders"], method = [RequestMethod.POST])
+    fun open(@HelloworldUser user: User,
+             @PathVariable("cartId") cartId: String) {
+        val id = orderApplicationService.create(user, cartId)
+        ResponseEntity.created(URI("http://localhst:8080/orders/${id}"))
     }
 }
--- external-api/src/main/kotlin/com/helloworld/order/v1/controller/OrderController.kt
+++ external-api/src/main/kotlin/com/helloworld/order/v1/controller/OrderController.kt
@@ -2,15 +2,13 @@ package com.helloworld.order.v1.controller
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController(value = "OrderControllerV1")
+@RequestMapping(
+        value = ["/order/orders"],
+        produces = ["application/vnd.helloworld.api.v1+json; charset=UTF-8"]
+)
 class OrderController {
-    @Value("\${spring.redis.timeout}")
-    lateinit var redis: String
-
-    @GetMapping("")
-    fun index() {
-        println(redis)
-    }
 }
