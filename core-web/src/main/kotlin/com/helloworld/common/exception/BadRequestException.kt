9a918c83e114f0cec528743e9d805176c96fc0d8
Jiwhun Kim <jiwhun.kim@outlook.com>
2021-02-27 13:12:00 +0900

    core-web exception handle

--- core-web/src/main/kotlin/com/helloworld/common/exception/BadRequestException.kt
+++
@@ -1,8 +0,0 @@
-package com.helloworld.common.exception
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
-class BadRequestException(override var code: String, override val type: String, override var message: String) : RuntimeException(message), DefaultException {
-}
--- core-web/src/main/kotlin/com/helloworld/common/exception/DefaultException.kt
+++
@@ -1,7 +0,0 @@
-package com.helloworld.common.exception
-
-interface DefaultException {
-    val code: String
-    val type: String
-    val message: String
-}
