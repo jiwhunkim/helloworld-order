704288a6251c248b9122b15f4b5a52f78b90e2b0
Jiwhun Kim <jiwhun.kim@outlook.com>
2021-03-17 23:19:58 +0900

    change enum value

--- domain-rds/src/main/kotlin/com/helloworld/domain/order/OrderPayDiscountEntity.kt
+++ domain-rds/src/main/kotlin/com/helloworld/domain/order/OrderPayDiscountEntity.kt
@@ -13,6 +13,7 @@ class OrderPayDiscountEntity(
         @Column
         var mappingName: String,
         @Column
+        @Enumerated(EnumType.STRING)
         var valueType: DiscountValueType,
         @Column
         var value: BigDecimal = BigDecimal.ZERO,
