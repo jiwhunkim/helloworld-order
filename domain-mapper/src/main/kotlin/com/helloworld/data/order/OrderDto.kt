6e236116af0dcde944f4017e7c56956dea3645d7
Jiwhun Kim <jiwhun.kim@outlook.com>
2021-03-17 23:09:18 +0900

    add payDiscount ModelMapper and dto

--- domain-mapper/src/main/kotlin/com/helloworld/data/order/OrderDto.kt
+++ domain-mapper/src/main/kotlin/com/helloworld/data/order/OrderDto.kt
@@ -5,20 +5,21 @@ import java.math.BigDecimal
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class OrderDto(
-        val id: Long = 0,
-        val deviceId: String,
-        val accountId: Long,
-        val cartId: String,
-        val status: String,
-        val orderUserContact: String,
-        val orderUserNickname: String,
-        val shop: OrderShopDto,
-        val amount: BigDecimal = BigDecimal.ZERO,
-        val salesAmount: BigDecimal = BigDecimal.ZERO,
-        val discountAmount: BigDecimal = BigDecimal.ZERO,
-        val totalAmount: BigDecimal = BigDecimal.ZERO,
-        val delivery: DeliveryDto
+    val id: Long = 0,
+    val deviceId: String,
+    val accountId: Long,
+    val cartId: String,
+    val status: String,
+    val orderUserContact: String,
+    val orderUserNickname: String,
+    val shop: OrderShopDto,
+    val amount: BigDecimal = BigDecimal.ZERO,
+    val salesAmount: BigDecimal = BigDecimal.ZERO,
+    val discountAmount: BigDecimal = BigDecimal.ZERO,
+    val totalAmount: BigDecimal = BigDecimal.ZERO,
+    val delivery: DeliveryDto
 ) {
     val lineItems: MutableList<LineItemDto> = mutableListOf()
     val cartDiscounts: MutableList<OrderCartDiscountDto> = mutableListOf()
+    val payDiscounts: MutableList<OrderPayDiscountDto> = mutableListOf()
 }
