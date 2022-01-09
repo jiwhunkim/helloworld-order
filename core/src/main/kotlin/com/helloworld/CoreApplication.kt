0c02a8b142d1bac022a19d8aa4c9474e40777f39
Jiwhun Kim <jiwhun.kim@outlook.com>
2022-01-09 16:38:28 +0900

    spring version up

--- core/src/main/kotlin/com/helloworld/CoreApplication.kt
+++ core/src/main/kotlin/com/helloworld/CoreApplication.kt
@@ -3,7 +3,6 @@ package com.helloworld
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-
 @SpringBootApplication
 class CoreApplication
 
